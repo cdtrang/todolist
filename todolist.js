efb70aa2baf93c6b743fbf6a44e121bf1ed7211c
Chinh Trang <chinhdtrang@gmail.com>
2016-04-05 19:26:27 -0500

    Initial Design

--- todolist.js
+++ todolist.js
@@ -1,19 +1,44 @@
+Tasks= new Mongo.Collection('tasks');
+
+Meteor.methods({
+  createTask: function(text) {
+    if (!Meteor.userId()) throw new Meteor.Error('not-authorized');
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.user()._id
+    });
+  },
+  deleteTask: function(taskId) {
+    // TODO Check the user
+      Tasks.remove(taskId);
+  }
+});
+
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+    Template.ToDoList.helpers({
     counter: function () {
       return Session.get('counter');
     }
-  });
+    });
 
-  Template.hello.events({
+    Template.ToDoList.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
+    });
+
+    Template.ToDoList.rendered = function() {
+    this.$('.datepicker').datepicker();
+    }
+    
+    
 }
 
 if (Meteor.isServer) {
